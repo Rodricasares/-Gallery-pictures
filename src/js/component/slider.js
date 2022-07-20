c8edfb0c15ca31addcc9e44be9ec37712c8f7d1c
Rodricasares <rodrigocantudo@gmail.com>
2022-07-20 08:11:55 +0000

    algun re-toque por acabar

---
+++ src/js/component/slider.js
@@ -0,0 +1,42 @@
+import React from "react";
+import images from "./images";
+import "../../styles/slider.css";
+import { motion } from "framer-motion";
+
+export const Slider = () => (
+  <div className="skill container ">
+    <div className="row ">
+      <div className="col text-secondary">
+        <div className="row mt-5 ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="80"
+            height="80"
+            fill="currentColor"
+            class="bi bi-images"
+            viewBox="0 0 16 16"
+          >
+            <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
+            <path d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2zM14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1zM2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1h-10z" />
+          </svg>
+        </div>
+        <h1 className="title text-center fw-bold my-3">Gallery pictures</h1>
+        <div className="row ">
+          <motion.div className="slider-container ">
+            <motion.div
+              className="slider"
+              drag="x"
+              dragConstraints={{ right: 0, left: -3500 }}
+            >
+              {images.map((image) => (
+                <motion.div className="item mt-5">
+                  <img src={image} alt="" className="rounded" />
+                </motion.div>
+              ))}
+            </motion.div>
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,33 +1,17 @@
 import React from "react";
 import rigoImage from "../../img/perfil.jpg";
 import "../../styles/home.css";
-import { About } from "../component/about";
-import { Header } from "../component/header";
-import { Project } from "../component/project";
-import { Skill } from "../component/skill";
-import { Contact } from "../component/contact";
-import { Nav } from "../component/nav";
+
+import { Slider } from "../component/slider";
+
 export const Home = () => (
   <div className="row justify-content-center mb-5">
 <link rel="preconnect" href="https://fonts.googleapis.com"/>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-<link href="https://fonts.googleapis.com/css2?family=Bowlby+One&family=NTR&family=Short+Stack&display=swap" rel="stylesheet"/>
-
-
-   <div className="container ">
-      <Header />
-    </div>
-    <div className="container-fluid about">
-      <About />
-    </div>
-     <div className="container-fluid project">
-      <Project />
-    </div>
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"/>
     <div className="container-fluid skill">
-      <Skill />
+      <Slider />
     </div>
-    <div className="container-fluid contact">
-      <Contact />
-    </div>
+
   </div>
 );
